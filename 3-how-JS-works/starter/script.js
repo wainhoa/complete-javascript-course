3f59224f49e5cbddcffe095942ab11d8c0d96ca7
Ainhoa Rebolledo Torrens <rebolledotorrens@gmail.com>
2020-04-10 14:23:47 +0200

    prueba

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,13 +1,27 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/* calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2020 - year);
+}
+
+var retirement = function(year) {
+    console.log(65 - (2020 - year));
+}
 
 
+var age = 33;
+console.log(age);
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
 
-
-
-
+foo();
+console.log(age);
 
 
 
@@ -42,7 +56,9 @@ function first() {
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
+/* 
+
 var a = 'Hello!';
 first();
 
@@ -58,20 +74,51 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a+d);
 }
-*/
+
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+*/
 
 
+//console.log(this);
+
+// calculateAge(1985);
+
+/*
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+} 
+
+*/
+
+var ainhoa = {
+    name: 'Ainhoa',
+    yearOfBirth: 1987,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2020 - this.yearOfBirth);
+   
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+ainhoa.calculateAge();
 
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
 
-
-
-
+mike.calculateAge = ainhoa.calculateAge;
+mike.calculateAge();
