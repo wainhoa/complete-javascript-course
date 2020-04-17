0232a12fc1f62a17eee9e44f8866265086716007
Ainhoa Torrens <ainhoa.torrens@intranet.ametsis.com>
2020-04-17 08:31:54 +0200

    viernes

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,77 @@
+// Function constructor //
+
+/*
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+//instances of the person object//
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
+
+
+
+// Object create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+
+var jane = Object.create(personProto, {
+
+    name: { value: 'Jane'},
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer'}
+
+});
+
+*/
+
+// Primitives vs objects 
+
+var a = 23;
+var b = a;
+
+a = 46;
+
+console.log(a);
+console.log(b);
+
+
+
+
+
